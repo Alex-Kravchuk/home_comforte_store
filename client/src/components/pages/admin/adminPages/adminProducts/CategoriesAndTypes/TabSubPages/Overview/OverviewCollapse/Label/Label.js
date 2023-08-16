801c4218ebb235d1cd1503ca5f3bd7a6bfd28273
Alex Kravchuk <sodasoad450@gmail.com>
2023-08-16 16:41:02 +0300

    Added input for change category name

--- client/src/components/pages/admin/adminPages/adminProducts/CategoriesAndTypes/TabSubPages/Overview/OverviewCollapse/Label/Label.js
+++ client/src/components/pages/admin/adminPages/adminProducts/CategoriesAndTypes/TabSubPages/Overview/OverviewCollapse/Label/Label.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Button, Tooltip } from "antd";
+import { Button, Input, Popconfirm, Tooltip } from "antd";
 
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
@@ -9,23 +9,69 @@ import {
   LabelWraper,
   LabelContainer,
   LabelButtonGroupe,
+  LabelEditInputWrapper,
 } from "./Label.styled";
 
-const Label = ({ children }) => {
+const Label = ({ confirm, cancel, children }) => {
+  const [showEditFiled, setShowEditField] = useState(false);
+
+  const stopPropagationHandler = (e) => e.stopPropagation();
   return (
     <LabelWraper>
       <LabelContainer>
         <LabelName>{children}</LabelName>
+
         <LabelButtonGroupe>
-          <Tooltip title="Edit" placement="left">
-            <Button>
+          <LabelEditInputWrapper
+            edit={showEditFiled}
+            onClick={stopPropagationHandler}
+          >
+            <Input
+              placeholder="Please input a new name"
+              defaultValue={children}
+            />
+            <Popconfirm
+              title="Save"
+              description={`Are you sure to save a new name?`}
+              onConfirm={confirm}
+              onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button>Save</Button>
+            </Popconfirm>
+          </LabelEditInputWrapper>
+          <Tooltip
+            title="Edit"
+            placement="left"
+            onClick={stopPropagationHandler}
+          >
+            <Button onClick={() => setShowEditField((state) => !state)}>
               <ModeEditOutlineOutlinedIcon />
             </Button>
           </Tooltip>
-          <Tooltip title="Delete" placement="right">
-            <Button>
-              <DeleteOutlineOutlinedIcon />
-            </Button>
+          <Tooltip
+            title="Delete"
+            placement="right"
+            onClick={stopPropagationHandler}
+          >
+            <Popconfirm
+              title="Delete"
+              description={
+                <div>
+                  Are you sure to delete
+                  <strong> {children}</strong>?
+                </div>
+              }
+              onConfirm={confirm}
+              onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button>
+                <DeleteOutlineOutlinedIcon />
+              </Button>
+            </Popconfirm>
           </Tooltip>
         </LabelButtonGroupe>
       </LabelContainer>
--- client/src/components/pages/admin/adminPages/adminProducts/CategoriesAndTypes/TabSubPages/Overview/OverviewCollapse/Label/Label.styled.js
+++ client/src/components/pages/admin/adminPages/adminProducts/CategoriesAndTypes/TabSubPages/Overview/OverviewCollapse/Label/Label.styled.js
@@ -1,7 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const LabelWraper = styled.div`
-`;
+export const LabelWraper = styled.div``;
 export const LabelContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -17,3 +16,24 @@ export const LabelButtonGroupe = styled.div`
   }
 `;
 
+export const LabelEditInputWrapper = styled.div`
+  width: 350px;
+  display: flex;
+  position: relative;
+  visibility: hidden;
+  opacity: 0;
+  left: 0;
+  transition: all 0.3s ease;
+
+  button {
+    margin: 0 !important;
+  }
+
+  ${({ edit }) =>
+    edit &&
+    css`
+      visibility: visible;
+      opacity: 1;
+      left: -100px;
+    `};
+`;
